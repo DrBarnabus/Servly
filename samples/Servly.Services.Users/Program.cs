80a2413ed1033c0ad755a64e3c771aba64e67577
DrBarnabus <16451892+DrBarnabus@users.noreply.github.com>
2021-03-10 17:58:43 +0000

    docs(samples): add an example service
     This commit adds an example project called Servly.Services.Users which will
     be used for a combination of testing purposes as well as providing an example
     of how to use the project in an AspNetCore Application.

---
+++ samples/Servly.Services.Users/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Servly.Core;
+using Servly.Core.Options;
+using System.Threading.Tasks;
+
+namespace Servly.Services.Users
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await CreateHostBuilder(args).Build().RunAsync();
+        }
+
+        private static IServlyHostBuilder CreateHostBuilder(string[] args)
+        {
+            return ServlyHost.CreateDefaultBuilder(args)
+                .ConfigureServices(services => services.AddRouting())
+                .Configure(app => app
+                    .UseRouting()
+                    .UseEndpoints(endpoints =>
+                    {
+                        endpoints.MapGet("/", async context =>
+                        {
+                            var servlyOptions = context.RequestServices.GetRequiredService<IOptions<ServlyOptions>>();
+                            await context.Response.WriteAsync($"Hello World from {servlyOptions.Value.ServiceName}!");
+                        });
+                    }));
+        }
+    }
+}
