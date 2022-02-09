ea082fae143480af94e9c3be6db1ef9f83eedb66
DrBarnabus <16451892+DrBarnabus@users.noreply.github.com>
2022-02-09 22:12:37 +0000

    feat(core): add Utilities class for future use
    Adds a Utilities for future use with two methods, one to return the assembly name and another to return the assembly informational version.

---
+++ src/Core/src/Servly.Core/Utilities.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using Servly.Core.Exceptions;
+
+namespace Servly.Core;
+
+public static class Utilities
+{
+    /// <summary>
+    ///     Returns the name of the entry assembly or the assembly that has been passed in.
+    /// </summary>
+    /// <param name="assembly">The assembly to return the name of, if not specified `GetEntryAssembly` is used instead.</param>
+    /// <returns>The simple name of the assembly.</returns>
+    /// <exception cref="ServlyException">In the event the assembly's name can't be located.</exception>
+    public static string GetAssemblyName(Assembly? assembly = null)
+    {
+        assembly ??= Assembly.GetEntryAssembly();
+        return assembly?.GetName().Name ?? throw new ServlyException("Unable to locate Assembly's Name.");
+    }
+
+    /// <summary>
+    ///     Returns the value of <see cref="AssemblyInformationalVersionAttribute" /> applied to the entry assembly or the assembly that has been passed in.
+    /// </summary>
+    /// <param name="assembly">The assembly to return the informational version of, if not specified `GetEntryAssembly` is used instead.</param>
+    /// <returns>The value of the assembly's informational version or "unknown" in the event it can't be located.</returns>
+    public static string GetAssemblyInformationalVersion(Assembly? assembly = null)
+    {
+        assembly ??= Assembly.GetEntryAssembly();
+        var attribute = assembly?.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+        return attribute?.InformationalVersion.Insert(0, "v") ?? "unknown";
+    }
+}
---
+++ src/Core/test/Servly.Core.UnitTests/UtilitiesTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using Shouldly;
+using Xunit;
+
+namespace Servly.Core.UnitTests;
+
+public class UtilitiesTests
+{
+    [Fact]
+    public void GetAssemblyName_ShouldReturnCorrectNameWhenNoAssemblySpecified()
+    {
+        // Act
+        string assemblyName = Utilities.GetAssemblyName();
+
+        // Assert - This assertion can't be hardcoded as it depends on the test runner.
+        assemblyName.ShouldBe(Assembly.GetEntryAssembly()?.GetName().Name);
+    }
+
+    [Fact]
+    public void GetAssemblyName_ShouldReturnCorrectNameWhenAssemblySpecified()
+    {
+        // Act
+        string assemblyName = Utilities.GetAssemblyName(typeof(UtilitiesTests).Assembly);
+
+        // Assert
+        assemblyName.ShouldBe("Servly.Core.UnitTests");
+    }
+
+    [Fact]
+    public void GetAssemblyInformationalVersion_ShouldReturnCorrectVersion()
+    {
+        // Act
+        string assemblyVersion = Utilities.GetAssemblyInformationalVersion(typeof(UtilitiesTests).Assembly);
+
+        // Assert - This relies on the version applied to the Servly.Core.UnitTests assembly.
+        assemblyVersion.ShouldBe("v1.0.0");
+    }
+}
