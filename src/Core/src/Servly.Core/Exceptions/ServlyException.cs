b1cf8f11e20eb34f35f3ed04749a97b4c7bc639f
DrBarnabus <16451892+DrBarnabus@users.noreply.github.com>
2022-02-09 21:39:26 +0000

    feat(core): add ServlyException class for future use

---
+++ src/Core/src/Servly.Core/Exceptions/ServlyException.cs
@@ -0,0 +1,16 @@
+﻿namespace Servly.Core.Exceptions;
+
+public class ServlyException : Exception
+{
+    public ServlyException(string? message)
+        : base(message)
+    {
+    }
+
+    public ServlyException(string? message, Exception? innerException)
+        : base(message, innerException)
+    {
+    }
+
+    public virtual string Code { get; } = "default";
+}
---
+++ src/Core/test/Servly.Core.UnitTests/Exceptions/ServlyExceptionTests.cs
@@ -0,0 +1,33 @@
+﻿using Servly.Core.Exceptions;
+using Shouldly;
+using Xunit;
+
+namespace Servly.Core.UnitTests.Exceptions;
+
+public class ServlyExceptionTests
+{
+    [Fact]
+    public void ShouldHaveCodeOfDefault()
+    {
+        var subject = new ServlyException(null);
+        subject.Code.ShouldBe("default");
+    }
+
+    [Fact]
+    public void ShouldHaveCorrectMessage()
+    {
+        var subject = new ServlyException("Expected Message");
+        subject.Message.ShouldBe("Expected Message");
+    }
+
+    [Fact]
+    public void ShouldHaveCorrectInnerException()
+    {
+        var exceptedInner = new Exception("Expected Inner");
+
+        var subject = new ServlyException(null, exceptedInner);
+        subject.InnerException.ShouldNotBeNull();
+        subject.InnerException.ShouldBe(exceptedInner);
+        subject.InnerException.Message.ShouldBe(exceptedInner.Message);
+    }
+}
