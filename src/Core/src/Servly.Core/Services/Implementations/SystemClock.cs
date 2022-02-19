c290c98a48ff90d94a79e591b07409f0e192cd65
DrBarnabus <16451892+DrBarnabus@users.noreply.github.com>
2022-02-19 15:18:51 +0000

    feat(core): add IClock interface for Unit Testing

---
+++ src/Core/src/Servly.Core/Services/Implementations/SystemClock.cs
@@ -0,0 +1,14 @@
+﻿using Servly.Core.Services.Interfaces;
+
+namespace Servly.Core.Services.Implementations;
+
+/// <summary>
+///     Provides access to the system clock for consumers of <see cref="IClock" />.
+/// </summary>
+public class SystemClock : IClock
+{
+    /// <summary>
+    ///     Retrieves the current system time in UTC.
+    /// </summary>
+    public DateTimeOffset UtcNow => DateTimeOffset.UtcNow;
+}
---
+++ src/Core/src/Servly.Core/Services/Interfaces/IClock.cs
@@ -0,0 +1,12 @@
+﻿namespace Servly.Core.Services.Interfaces;
+
+/// <summary>
+///     Provides an abstraction of the system clock to facilitate unit testing.
+/// </summary>
+public interface IClock
+{
+    /// <summary>
+    ///     Retrieves the current time in UTC.
+    /// </summary>
+    public DateTimeOffset UtcNow { get; }
+}
