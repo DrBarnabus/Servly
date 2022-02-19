ce9b7ee69695394de15bf4671e034bb0646be0ab
DrBarnabus <16451892+DrBarnabus@users.noreply.github.com>
2022-02-19 21:55:23 +0000

    feat(authentication): add AspNetCore middleware to set AuthenticationContext from Authentication
    This commit adds a authentication package specifically for AspNetCore that implements `IServiceCollection` and `IApplicationBuilder` extensions for adding `AuthenticationContext` into a AspNetCore project and initializing it via a http middleware.
    
    This middleware provides a configurable interface to set an authentication context. It is configurable so that users can subclass `AuthenticationContext` to add their own "interesting" fields and extract them from the authentication.

---
+++ src/Authentication/src/Servly.Authentication.AspNetCore/Middleware/AuthenticationContextMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Servly.Core;
+
+namespace Servly.Authentication.AspNetCore.Middleware;
+
+public class AuthenticationContextMiddleware<TState> : IMiddleware
+    where TState : class, IAuthenticationContextState, new()
+{
+    private readonly ILogger<AuthenticationContextMiddleware<TState>> _logger;
+    private readonly Action<TState, HttpContext> _setupContext;
+
+    public AuthenticationContextMiddleware(ILogger<AuthenticationContextMiddleware<TState>> logger, Action<TState, HttpContext> setupContext)
+    {
+        Guard.Assert(logger is not null, $"Logger cannot be null");
+        Guard.Assert(setupContext is not null, $"SetupContext cannot be null");
+
+        _logger = logger;
+        _setupContext = setupContext;
+    }
+
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        if (context.User.Identity is not { IsAuthenticated: true })
+        {
+            _logger.LogTrace("Request is not authenticated, unable to set authentication context");
+            await next(context);
+            return;
+        }
+
+        _logger.LogTrace("Request is authenticated, initializing authentication context from Identity in HttpContext");
+
+        var authenticationContext = context.RequestServices.GetRequiredService<IAuthenticationContext<TState>>();
+        using var _ = authenticationContext.SetContext(state =>
+        {
+            state.IsAuthenticated = true;
+            _setupContext(state, context);
+        });
+
+        await next(context);
+    }
+}
---
+++ src/Authentication/test/Servly.Authentication.AspNetCore.UnitTests/Middleware/AuthenticationContextMiddlewareTests.cs
@@ -0,0 +1,127 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using Servly.Authentication.AspNetCore.Middleware;
+using Shouldly;
+using Xunit;
+
+namespace Servly.Authentication.AspNetCore.UnitTests.Middleware;
+
+public class AuthenticationContextMiddlewareTests
+{
+    [Fact]
+    public void ShouldThrowIfConstructedWithNullLogger()
+    {
+        Should.Throw<ArgumentException>(() =>
+            new AuthenticationContextMiddleware<AuthenticationContextState>(null!, (_, _) => { }));
+    }
+
+    [Fact]
+    public void ShouldThrowIfConstructedWithNullAction()
+    {
+        var nullLogger = NullLogger<AuthenticationContextMiddleware<AuthenticationContextState>>.Instance;
+        Should.Throw<ArgumentException>(() =>
+            new AuthenticationContextMiddleware<AuthenticationContextState>(nullLogger, null!));
+    }
+
+    [Fact]
+    public async Task ShouldNotModifyTheAuthenticationContextWhenHttpContextIdentityIsNotAuthenticated()
+    {
+        // Setup
+        var nullLogger = NullLogger<AuthenticationContextMiddleware<AuthenticationContextState>>.Instance;
+        var sut = new AuthenticationContextMiddleware<AuthenticationContextState>(nullLogger, (state, _) =>
+        {
+            state.SubjectId = Guid.Parse("77268032-39a0-488c-81de-493452fdc29b");
+        });
+
+        var originalState = new AuthenticationContextState
+        {
+            IsAuthenticated = false, SubjectId = Guid.Parse("a09a42bc-aae0-4e70-a4a2-f880d4c9ce6c")
+        };
+        var authenticationContext = new AuthenticationContext<AuthenticationContextState>(originalState);
+
+        var serviceProviderMock = new Mock<IServiceProvider>();
+        serviceProviderMock.Setup(x => x.GetService(typeof(IAuthenticationContext<AuthenticationContextState>)))
+            .Returns(authenticationContext);
+
+        var httpContextMock = new Mock<HttpContext>();
+        httpContextMock.Setup(x => x.User).Returns(new ClaimsPrincipal());
+        httpContextMock.Setup(x => x.RequestServices).Returns(serviceProviderMock.Object);
+
+        var requestDelegate = new RequestDelegate(_ =>
+        {
+            var inProgressState = authenticationContext.GetState();
+            inProgressState.ShouldBeSameAs(originalState);
+            inProgressState.ShouldNotBeNull();
+            inProgressState.IsAuthenticated.ShouldBeFalse();
+            inProgressState.SubjectId.ShouldBe(Guid.Parse("a09a42bc-aae0-4e70-a4a2-f880d4c9ce6c"));
+
+            return Task.FromResult(0);
+        });
+
+        // Action
+        await sut.InvokeAsync(httpContextMock.Object, requestDelegate);
+        var newState = authenticationContext.GetState();
+
+        // Assert & Validate
+        serviceProviderMock.Verify(x => x.GetService(typeof(IAuthenticationContext<AuthenticationContextState>)), Times.Never);
+        newState.ShouldBeSameAs(originalState);
+        newState.ShouldNotBeNull();
+        newState.IsAuthenticated.ShouldBeFalse();
+        newState.SubjectId.ShouldBe(Guid.Parse("a09a42bc-aae0-4e70-a4a2-f880d4c9ce6c"));
+    }
+
+    [Fact]
+    public async Task ShouldModifyTheAuthenticationContextWhenHttpContextIdentityIsAuthenticated()
+    {
+        // Setup
+        var nullLogger = NullLogger<AuthenticationContextMiddleware<AuthenticationContextState>>.Instance;
+        var sut = new AuthenticationContextMiddleware<AuthenticationContextState>(nullLogger, (state, _) =>
+        {
+            state.SubjectId = Guid.Parse("77268032-39a0-488c-81de-493452fdc29b");
+        });
+
+        var originalState = new AuthenticationContextState
+        {
+            IsAuthenticated = false, SubjectId = Guid.Parse("a09a42bc-aae0-4e70-a4a2-f880d4c9ce6c")
+        };
+        var authenticationContext = new AuthenticationContext<AuthenticationContextState>(originalState);
+
+        var serviceProviderMock = new Mock<IServiceProvider>();
+        serviceProviderMock.Setup(x => x.GetService(typeof(IAuthenticationContext<AuthenticationContextState>)))
+            .Returns(authenticationContext);
+
+        var httpContextMock = new Mock<HttpContext>();
+        httpContextMock.Setup(x => x.User).Returns(new ClaimsPrincipal(new List<ClaimsIdentity>
+        {
+            new(new List<Claim>
+            {
+                new("ClaimType", "ClaimValue")
+            }, "UnitTestAuthentication")
+        }));
+        httpContextMock.Setup(x => x.RequestServices).Returns(serviceProviderMock.Object);
+
+        var requestDelegate = new RequestDelegate(_ =>
+        {
+            var inProgressState = authenticationContext.GetState();
+            inProgressState.ShouldNotBeSameAs(originalState);
+            inProgressState.ShouldNotBeNull();
+            inProgressState.IsAuthenticated.ShouldBeTrue();
+            inProgressState.SubjectId.ShouldBe(Guid.Parse("77268032-39a0-488c-81de-493452fdc29b"));
+
+            return Task.FromResult(0);
+        });
+
+        // Action
+        await sut.InvokeAsync(httpContextMock.Object, requestDelegate);
+        var newState = authenticationContext.GetState();
+
+        // Assert & Validate
+        serviceProviderMock.Verify(x => x.GetService(typeof(IAuthenticationContext<AuthenticationContextState>)), Times.Once);
+        newState.ShouldBeSameAs(originalState);
+        newState.ShouldNotBeNull();
+        newState.IsAuthenticated.ShouldBeFalse();
+        newState.SubjectId.ShouldBe(Guid.Parse("a09a42bc-aae0-4e70-a4a2-f880d4c9ce6c"));
+    }
+}
