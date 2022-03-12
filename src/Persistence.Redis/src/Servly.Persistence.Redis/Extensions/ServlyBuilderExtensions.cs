561251c98330ea34ccec989f7c9705f1694d703c
DrBarnabus <16451892+DrBarnabus@users.noreply.github.com>
2022-03-12 22:35:46 +0000

    feat(persistence.redis): implement ServlyBuilder for type registration

---
+++ src/Persistence.Redis/src/Servly.Persistence.Redis/Extensions/ServlyBuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Servly.Core;
+using StackExchange.Redis;
+
+namespace Servly.Persistence.Redis.Extensions;
+
+public static class ServlyBuilderExtensions
+{
+    private const string RedisProviderModuleName = "Persistence.Redis";
+    private const string DefaultSectionName = "Servly:Redis";
+
+    public static IServlyBuilder AddRedis(this IServlyBuilder builder, string configurationSectionName = DefaultSectionName, bool distributedCache = true)
+    {
+        if (builder.TryRegisterModule(RedisProviderModuleName))
+            return builder;
+
+        builder.AddOptions<RedisOptions>(configurationSectionName);
+        var redisOptions = builder.GetOptions<RedisOptions>();
+
+        builder.Services
+            .AddSingleton<IConnectionMultiplexer>(sp => ConnectionMultiplexer.Connect(redisOptions.ConnectionString))
+            .AddSingleton<IRedisProvider, RedisProvider>();
+
+        if (distributedCache)
+            builder.Services.AddStackExchangeRedisCache(cacheOptions =>
+            {
+                cacheOptions.Configuration = redisOptions.ConnectionString;
+                cacheOptions.InstanceName = redisOptions.InstanceName;
+            });
+
+        return builder;
+    }
+}
